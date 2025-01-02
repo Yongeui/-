f748a121524b35d00b32ec277ffb06e4cd7be6b1
Yongeui <113080957+Yongeui@users.noreply.github.com>
2025-01-02 10:32:46 +0900

    Create sec01.exam05.java

---
+++ Practise/Java/chap01/sec01.exam05.java
@@ -0,0 +1,18 @@
+package sec01.exam05;
+
+public class VariableinitializationExample {
+
+	public static void main(String[] args) {
+		
+		int hour = 3;
+		int minute =5;
+		System.out.println( hour + "시간" + minute + "분" );
+		
+		int totalMinute = (hour*60) + minute;
+		System.out.println("총" + totalMinute + "분");
+
+	}
+
+}
+
+//result : 3시간 5분, 총185분
