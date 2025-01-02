aad17022e60e8494b78da0adfff3b1bbee530fd7
Yongeui <113080957+Yongeui@users.noreply.github.com>
2025-01-02 10:48:15 +0900

    Create sec01.exam12.java

---
+++ Practise/Java/chap01/sec01.exam12.java
@@ -0,0 +1,20 @@
+package sec01.exam12;
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		
+		String name = "홍길동";
+		String job = "프로그래머";
+		
+		System.out.println(name);
+		System.out.println(job);
+		
+		
+	}
+	
+}
+
+/* result : 
+홍길동
+프로그래머 */
