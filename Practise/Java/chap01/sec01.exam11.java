bc824501f749d7deba5db591641d18ac2b5d36de
Yongeui <113080957+Yongeui@users.noreply.github.com>
2025-01-02 10:47:10 +0900

    Create sec01.exam11.java

---
+++ Practise/Java/chap01/sec01.exam11.java
@@ -0,0 +1,33 @@
+package sec01.exam11;
+
+public class CharExample {
+
+	public static void main(String[] args) {
+		
+		char c1 = 'A';
+		char c2 = 65;
+		char c3 = '\u0041';
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		
+		char c4 = '가';
+		char c5 = 44032;
+		char c6 = '\uAC00';
+		
+		
+		System.out.println(c4);
+		System.out.println(c5);
+		System.out.println(c6);
+		
+	}
+	
+}
+/*result : 
+A
+A
+A
+가
+가
+가*/
