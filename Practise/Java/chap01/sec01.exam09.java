5b2f935a1763af70d2d56d553e0dbdec57cd2590
Yongeui <113080957+Yongeui@users.noreply.github.com>
2025-01-02 10:37:31 +0900

    Create sec01.exam09.java

---
+++ Practise/Java/chap01/sec01.exam09.java
@@ -0,0 +1,20 @@
+package sec01.exam09;
+
+public class ByteExample {
+	
+	public static void main(String[] args) {
+		
+		byte var1 = -128;
+		byte var2 = 0;
+		byte var3 = 127;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+	}
+}
+
+*/ result :
+-128
+0
+127 */
