05284c26e59bb204d1cae186a0f82d159f33995d
Yongeui <113080957+Yongeui@users.noreply.github.com>
2025-01-02 11:09:05 +0900

    Create sec01.exam19.java

---
+++ Practise/Java/chap01/sec01.exam19.java
@@ -0,0 +1,18 @@
+package sec01.exam19;
+
+public class LongOpertationExample {
+
+	public static void main(String[] args) {
+		
+		byte value1 = 10;
+		int value2 = 100;
+		long value3 = 1000;
+		
+		long result = value1 + value2 + value3;
+		
+		System.out.println(result);
+		
+	}
+}
+
+// reuslt : 1110
