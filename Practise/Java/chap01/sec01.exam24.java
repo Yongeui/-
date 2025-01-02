0161f280b5d16d956ceb790b84e5f0eac4a4be58
Yongeui <113080957+Yongeui@users.noreply.github.com>
2025-01-02 11:18:56 +0900

    Create sec01.exam24.java

---
+++ Practise/Java/chap01/sec01.exam24.java
@@ -0,0 +1,22 @@
+package sec01.exam24;
+
+public class KeyCodeExample {
+
+	public static void main(String[] args) throws Exception {
+		
+		int KeyCode;
+		
+		KeyCode = System.in.read();
+		System.out.println("KeyCode:" + KeyCode);
+		 
+		KeyCode = System.in.read();
+		System.out.println("KeyCode:" + KeyCode);
+		
+		KeyCode = System.in.read();
+		System.out.println("KeyCode:" + KeyCode);
+		
+		
+	}
+	
+}
+//사용자의 입력에 해당하는 아스키코드를 출력한다.
